fe101105ed72bb2067b5ae787249592aeb7cd853
Giacomo R <gradaellig@protonmail.com>
2022-07-05 19:24:50 +0200

    Modificato il countdown

--- script/countdown.js
+++ script/countdown.js
@@ -1,41 +1,52 @@
 //COUNTDOWN 	|||
 //				VVV
 
-// Set the date we're counting down to
+var svolg = "Il gruppo di lettura si sta svolgendo";
+var agg = "Il countdown deve essere aggiornato. Se è passato troppo tempo, contatta l'amministratore del sito aprendo un'issue su github oppure manda una mail all'indirizzo presente su github"
+
+// Data del prossimo incontro
 var countDownDate = new Date("Jul 29, 2022 16:30:00").getTime();
 
-// Update the count down every 1 second
+// Aggiorna il countdown mostrato ogni 10 secondi
 var time = setInterval(function() {
 	
-	// Get today's date and time
+	// Data di oggi
 	var now = new Date().getTime();
 
-	// Find the distance between now and the count down date
+	// Distanza tra le date
 	var distance = countDownDate - now;
 
-	// Time calculations for days, hours, minutes and seconds
+	// Calcoli per sapere giorni, ore e minuti mancanti
 	var days = Math.floor(distance / (1000 * 60 * 60 * 24));
 	var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
 	var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
 	// var seconds = Math.floor((distance % (1000 * 60)) / 1000);   //INUTILIZZATO
 
-	// Display the result in the element with id="countdown"
+	var testo = "Mancano " + days + " giorni, " + hours + " ore, e " + minutes +  " minuti al prossimo gruppo di lettura<br>"
+
+	// Mostra su index e partecipa due testi diversi
 	if(document.URL.includes("index")) {
-		document.getElementById("countdown").innerHTML = "Mancano " + days + " giorni, " + hours + " ore, e " + minutes + " minuti al prossimo gruppo di lettura<br>La data potrebbe variare, se vuoi partecipare ed essere sicuro della data e dell'ora, contattaci in uno dei modi scritti in <a href=\"/pages/partecipa.html\">Partecipa</a>";
+		document.getElementById("countdown").innerHTML = testo + "La data potrebbe variare, se vuoi partecipare ed essere sicuro della data e dell'ora, contattaci in uno dei modi scritti in <a href=\"/pages/partecipa.html\">Partecipa</a>";
 	} else if (document.URL.includes("partecipa")) {
 		document.getElementById("countdown-part").innerHTML = "Mancano " + days + " giorni, " + hours + " ore, e " + minutes + " minuti al prossimo gruppo di lettura<br>La data potrebbe variare, se vuoi partecipare ed essere sicuro della data e dell'ora, contattaci in uno dei modi scritti qui. Ci troviamo l'ultimo venerdì del mese, solitamente alle 4:30";
 	}
 	
-
-	//document.getElementById("countdown").innerHTML = days + "d " + hours + "h " + minutes + "m ";
-
-	// If the count down is finished, write some text
-	if (distance < 0 && distance > -7500) {
+	// Parte di codice eseguita se distance<0 (seconda parte se sono passate più di tre ore dall'inizio dell'incontro)
+	if (distance < 0) {
+		if(distance < -10800000) {
+			if(document.URL.includes("index")) {
+				document.getElementById("countdown").innerHTML = agg;
+			} else if (document.URL.includes("partecipa")) {
+				document.getElementById("countdown-part").innerHTML = agg;
+			}
+		} else {
+			if(document.URL.includes("index")) {
+				document.getElementById("countdown").innerHTML = svolg;
+			} else if (document.URL.includes("partecipa")) {
+				document.getElementById("countdown-part").innerHTML = svolg;
+			}
+		}
 		clearInterval(time);
-		document.getElementById("countdown").innerHTML = "Il gruppo di lettura si sta svolgendo";
-	} else if (distance < 0) {
-		clearInterval(time);
-		document.getElementById("countdown").innerHTML = "Il countdown deve essere aggiornato. Se è passato troppo tempo, contatta l'amministratore del sito aprendo un'issue su github oppure manda una mail all'indirizzo presente su github";
 	}
 }, 1000);
 //document.getElementById("countdown").innerHTML = "La data per quest'incontro non è ancora stata stabilita";
